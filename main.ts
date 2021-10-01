76c575063e120728b8cb29dce3f0ccbe8a7a7eb2
SamCSDPS <89595260+SamCSDPS@users.noreply.github.com>
2021-10-01 09:05:47 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,44 @@
+let intensite = 0
+/**
+ * P1 = Rouge
+ * 
+ * P2 = Vert
+ * 
+ * P8 = Bleu
+ */
+function bleu () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function blanc () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function mauve () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function rouge () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+}
+function vert () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+}
+basic.forever(function () {
+    intensite = pins.analogReadPin(AnalogPin.P0)
+    serial.writeLine("" + (intensite))
+    if (intensite >= 700) {
+        blanc()
+    } else if (intensite < 700 && intensite >= 300) {
+        mauve()
+    } else {
+        rouge()
+    }
+})
